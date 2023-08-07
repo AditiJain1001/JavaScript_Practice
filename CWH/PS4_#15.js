25cf72ad651c46be7a031296509a2a24672cc97b
AditiJain1001 <codingwizard101@gmail.com>
2023-08-07 16:59:40 -0400

    Renamed file and finished 4/5 questions

---
+++ CWH/PS4_#15.js
@@ -0,0 +1,30 @@
+// Quick Quiz:
+let name2 = "Aditi";
+
+// for (i=0; i < (name2.length); i++) {
+//     console.log(name2[i]);
+// }
+
+for (a of name2) {
+  //for of loop iterates over the letters of a string not it's indicies such as name2[a]
+  console.log(a);
+}
+
+//Q1
+console.log('har"'.length); //Prints 4
+
+//Q2
+
+
+//Q3
+const food = "FruIt";
+console.log(food.toLowerCase());
+
+//Q4
+const str = "Total: Rupees 2907";
+console.log(str.slice(13))
+
+//Q5
+const lotion = "St'Ives";
+//Trick question - strings CANNOT BE chnaged; only replaced!
+console.log(lotion.replace("I", "O")); //produces a new string...
