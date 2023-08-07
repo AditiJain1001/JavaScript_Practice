9e1f0b91759c6fa8d4a642b34b0cb0d911369776
AditiJain1001 <codingwizard101@gmail.com>
2023-08-07 17:29:51 -0400

    Finished the last question in PS4_#15.js

--- CWH/PS4_#15.js
+++ CWH/PS4_#15.js
@@ -14,7 +14,11 @@ for (a of name2) {
 console.log('har"'.length); //Prints 4
 
 //Q2
-
+const clip = "long tissues biscuit";
+const word = clip.includes("tissue");
+const word2 = clip.startsWith("lol");
+const word3 = clip.endsWith("biscuit")
+console.log(word, word2, word3);
 
 //Q3
 const food = "FruIt";
